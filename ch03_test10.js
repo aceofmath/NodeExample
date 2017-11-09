920794ba10b001672c4c8b3227c0eb9b66d6d922
DESKTOP-SECS70T\Administrator <aceofmath@naver.com>
2017-11-09 17:00:32 +0900

    5단원 스터디

---
+++ ch03_test10.js
@@ -0,0 +1,145 @@
+/**
+ * 3장 Test 10
+ * 
+ * 배열 안의 모든 요소를 하나씩 확인하기
+ */
+
+var Users = [
+	{name:'소녀시대',age:20}
+	,{name:'걸스데이',age:22}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+	,{name:'티아라',age:23}
+];
+
+console.time('a_time');
+console.log('배열 요소의 수 : %d', Users.length);
+for (var i = 0; i < Users.length; i++) {
+	//console.log('배열 요소 #' + i + ' : %s', Users[i].name);
+} 
+console.timeEnd('a_time');
+
+console.time('b_time');
+console.log('\nforEach 구문 사용하기');
+Users.forEach(function(item, index) {
+	//console.log('배열 요소 #' + index + ' : %s', item.name);
+});
+console.timeEnd('b_time');
+
